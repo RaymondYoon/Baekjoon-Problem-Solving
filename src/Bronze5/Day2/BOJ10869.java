3cae8cd7c96e28ab726a1d22a3d95a4f5ce1c162
Raymond Yoon <yoonjunsang@naver.com>
2024-09-10 16:10:51 +0900

    난이도 별 파일 생성

--- src/Bronze5/Day2/BOJ10869.java
+++ src/Bronze5/Day2/BOJ10869.java
@@ -1,12 +1,16 @@
-package Day1;
+package Bronze5.Day2;
 
 import java.util.Scanner;
 
-public class BOJ1001 {
+public class BOJ10869 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
+        System.out.println(A+B);
         System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
     }
 }
