09d110ff4b485afbccbe61a786dcfcc39861ba21
Raymond Yoon <yoonjunsang@naver.com>
2024-09-10 17:52:15 +0900

    새로운 문제들 풀이

---
+++ src/Bronze5/Day9/BOJ10951.java
@@ -0,0 +1,16 @@
+package Bronze5.Day9;
+
+import java.util.Scanner;
+
+public class BOJ10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
+//sc.hasNext() 가 끝을 알 수 없을때 입력이 더 이상 들어오지 않을 때
+// 자동으로 종료되는 방식.
