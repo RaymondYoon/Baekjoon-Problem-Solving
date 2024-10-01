120f3392fe10af99b0555f35ce2661ab8f492812
Raymond Yoon <yoonjunsang@naver.com>
2024-10-02 00:31:48 +0900

    브론즈5 문제들 끝

---
+++ src/Bronze5/Day20/BOJ4101.java
@@ -0,0 +1,17 @@
+package Bronze5.Day20;
+
+import java.util.Scanner;
+
+public class BOJ4101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            if(N==0 && M==0)break;
+            else if(N>M) System.out.println("Yes");
+            else System.out.println("No");
+        }
+
+    }
+}
