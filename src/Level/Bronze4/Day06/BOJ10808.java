2ffd7a76376befa3af0a4aed9670b1fd380feea7
Raymond Yoon <yoonjunsang@naver.com>
2024-10-26 23:52:07 +0900

    Math.min 작은 수 더하는 문제

---
+++ src/Level/Bronze4/Day06/BOJ10808.java
@@ -0,0 +1,19 @@
+package Level.Bronze4.Day06;
+
+import java.util.Scanner;
+
+public class BOJ10808 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+        int J = sc.nextInt();
+        int H = sc.nextInt();
+        int min_buger = Math.min(S,Math.min(J, H));
+
+        int cola = sc.nextInt();
+        int cider = sc.nextInt();
+        int min_drink = Math.min(cola,cider);
+
+        System.out.println(min_buger + min_drink - 50);
+    }
+}
