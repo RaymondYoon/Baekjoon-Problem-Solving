274679402763be237ba38e48979b8cf047909203
Raymond Yoon <yoonjunsang@naver.com>
2025-01-04 01:36:10 +0900

    방학숙제문제 /리팩토링(Math클래스 사용)

--- src/Level/Bronze4/Day10/BOJ5532.java
+++ src/Level/Bronze4/Day10/BOJ5532.java
@@ -5,56 +5,17 @@ import java.util.Scanner;
 public class BOJ5532 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int L = sc.nextInt(); //총 방학 일
-        int A = sc.nextInt(); //국어 총 페이지 수
-        int B = sc.nextInt(); //수학 총 페이지 수
-        int C = sc.nextInt(); //하루 국어 페이지 수
-        int D = sc.nextInt(); //하루 수학 페이지 수
+        int L = sc.nextInt(); // 총 방학 일
+        int A = sc.nextInt(); // 국어 총 페이지 수
+        int B = sc.nextInt(); // 수학 총 페이지 수
+        int C = sc.nextInt(); // 하루 국어 페이지 수
+        int D = sc.nextInt(); // 하루 수학 페이지 수
 
-        if (A%C!=0 & B%D!=0){
-            if (A/C > B/D) {
-                System.out.println(L - (A / C + 1));
-            }
-            else if (A/C < B/D){
-                System.out.println(L - (B / D + 1));
-            }
-            else if (A/C == B/D){
-                System.out.println(L - (A / C + 1));
-            }
-        }
-        else if (A%C==0 & B%D!=0){
-            if (A/C > B/D) {
-                System.out.println(L - (A / C));
-            }
-            else if (A/C < B/D){
-                System.out.println(L - (B / D + 1));
-            }
-            else if (A/C == B/D){
-                    System.out.println(L - (B / D + 1));
-            }
-        }
+        int koreanDays = (int) Math.ceil((double) A / C);
+        int mathDays = (int) Math.ceil((double) B / D);
 
-        else if (A%C!=0 & B%D==0){
-            if (A/C > B/D) {
-                System.out.println(L - (A / C + 1));
-            }
-            else if (A/C < B/D){
-                System.out.println(L - (B / D));
-            }
-            else if (A/C == B/D){
-                    System.out.println(L - (A / C + 1));
-            }
-        }
-        else if (A%C==0 & B%D==0){
-            if (A/C > B/D) {
-                System.out.println(L - (A / C));
-            }
-            else if (A/C < B/D){
-                System.out.println(L - (B / D));
-            }
-            else if (A/C == B/D){
-                System.out.println(L - (A / C));
-            }
-        }
+        int maxDays = Math.max(koreanDays, mathDays);
+        System.out.println(L - maxDays);
     }
 }
+
