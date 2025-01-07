a9b6acd22a6e00d22b481ec8f1c634a928ace23d
Raymond Yoon <yoonjunsang@naver.com>
2025-01-08 01:02:26 +0900

    모음의 개수 두가지 풀이,toCharArray

--- src/Level/Bronze4/Day11/BOJ1264.java
+++ src/Level/Bronze4/Day11/BOJ1264.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class BOJ1264 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+
+        int count = 0;
+        for (char c : word.toCharArray()){
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
+/*
+package Level.Bronze4.Day11;
+
+import java.util.Scanner;
+
+public class BOJ1264 {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         scanner.close();
@@ -18,3 +39,5 @@ public class BOJ1264 {
         System.out.println(count);
     }
 }
+
+*/
