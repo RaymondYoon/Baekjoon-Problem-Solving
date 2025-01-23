b91abad30d996261e1338a5baa37d441c75ef0ce
Raymond Yoon <yoonjunsang@naver.com>
2025-01-23 19:45:43 +0900

    double 사용

---
+++ src/Level/Bronze4/Day19/BOJ16486.java
@@ -0,0 +1,13 @@
+package Level.Bronze4.Day19;
+
+import java.util.Scanner;
+
+public class BOJ16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        double c = 2*3.141592*d2;
+        System.out.println(2*d1 + c);
+    }
+}
