09422f6bfcf1e1b3ddd6624ab93062f927f157f0
Raymond Yoon <yoonjunsang@naver.com>
2025-02-05 23:49:05 +0900

    두 수의 연산값 비교하기

---
+++ src/Level/Bronze3/Day3/BOJ2525.java
@@ -0,0 +1,4 @@
+package Level.Bronze3.Day3;
+
+public class BOJ2525 {
+}
--- src/Programmers/lv0/lv0_1.java
+++ src/Programmers/lv0/lv0_1.java
@@ -1,2 +1,18 @@
-package Programmers.lv0;public class lv0_1 {
+package Programmers.lv0;
+import java.util.*;
+
+public class lv0_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        int result = Math.max(
+                Integer.parseInt(new StringBuilder().append(a).append(b).toString()),
+                2 * a * b
+        );
+
+        System.out.println(result);
+    }
 }
