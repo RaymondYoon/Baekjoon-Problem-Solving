2e6757d8fdf660853baab9b9ed92b15d9aee1585
Raymond Yoon <yoonjunsang@naver.com>
2025-04-02 16:45:16 +0900

    solve: BOJ 11718 입력 받은 대로 출력

---
+++ src/Level/Bronze3/Day8/BOJ11719.java
@@ -0,0 +1,13 @@
+package Level.Bronze3.Day8;
+
+import java.util.Scanner;
+
+public class BOJ11719 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(line);
+        }
+    }
+}
